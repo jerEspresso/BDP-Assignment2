1f4635fe76cdeec98a09b3ac564cc337805100a0
Jeremy Xiao <s3829221@student.rmit.edu.au>
2021-09-29 18:36:44 +1000

    initial commit

---
+++ Task1/src/main/java/s3829221/Task1/App.java
@@ -0,0 +1,13 @@
+package s3829221.Task1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Task2/src/main/java/s3829221/Task2/App.java
@@ -0,0 +1,13 @@
+package s3829221.Task2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
