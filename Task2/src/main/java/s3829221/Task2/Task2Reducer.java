a18397052febfba0cf117e5078449976087fe3d5
Jeremy Xiao <s3829221@student.rmit.edu.au>
2021-10-02 15:10:56 +1000

    Task 2 complete

--- Task2/src/main/java/s3829221/Task2/Task2Reducer.java
+++ Task2/src/main/java/s3829221/Task2/Task2Reducer.java
@@ -8,14 +8,39 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 public class Task2Reducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
 
+	private DoubleWritable total = new DoubleWritable();
+	private DoubleWritable relativeFreq = new DoubleWritable();
+	private String currentWord = null;
+
 	@Override
-	protected void reduce(Text keyIn, Iterable<DoubleWritable> valuesIn, Context context) throws IOException, InterruptedException {
-		int valueOut = 0;
-		
-		// Aggregate values of the same key
+	protected void reduce(Text keyIn, Iterable<DoubleWritable> valuesIn, Context context)
+			throws IOException, InterruptedException {
+		String[] wordPair = keyIn.toString().split(" ");
+		String leftWord = wordPair[0];
+		String rightWord = wordPair[1];
+
+		// Aggregate all (A, *) keys
+		if (rightWord.equals("*")) {
+			if (leftWord.equals(currentWord))
+				total.set(total.get() + getTotal(valuesIn));
+			// Clear the total when the left word changes
+			else {
+				currentWord = leftWord;
+				total.set(0);
+				total.set(getTotal(valuesIn));
+			}
+		// Calculate the relative frequency for each word pair
+		} else {
+			relativeFreq.set(getTotal(valuesIn) / total.get());
+			context.write(keyIn, relativeFreq);
+		}
+	}
+
+	private double getTotal(Iterable<DoubleWritable> valuesIn) {
+		double count = 0;
 		for (DoubleWritable value : valuesIn)
-			valueOut += value.get();
-		
-		context.write(keyIn, new DoubleWritable(valueOut));
+			count += value.get();
+
+		return count;
 	}
 }
